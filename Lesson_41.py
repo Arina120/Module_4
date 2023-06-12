c9b6ab4d7e743b7b95a242328bd2c4f07cd637cc
Arina120 <arina.matanskaya@mail.ru>
2023-06-12 19:27:59 +0300

    added file for les 41

---
+++ Lesson_41.py
@@ -0,0 +1,19 @@
+# SET {}
+# def strcount(s):
+#     for el in set(s):
+#         counter = 0
+#         for sub_el in s:
+#             if sub_el == el:
+#                 counter += 1
+#         print(el, counter)
+# strcount('aaabcd-5')
+# print(set('aaabcd-5'))
+
+def strcount(s):
+    el_counter = {}
+    for el in s:
+        el_counter[el] = el_counter.get(el, 0) + 1
+    for el, count in el_counter.items():
+        print(el, count)
+strcount('aaabcd-5')
+print(set('aaabcd-5'))
