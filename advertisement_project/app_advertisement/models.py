41deda7a02640272e8c595bbd28df06f136fceba
Arina120 <arina.matanskaya@mail.ru>
2023-08-17 12:57:19 +0300

    updated administration panel

--- advertisement_project/app_advertisement/models.py
+++ advertisement_project/app_advertisement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
@@ -10,6 +11,17 @@ class Advertisement(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
     
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_time.date() == timezone.now().date():
+            created_time_2 = self.created_time.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time_2
+            )
+        return self.created_time.strftime('%d.%m.%Y в %H:%M:%S')
+
+
     class Meta:
         db_table = 'advertisements'
 
