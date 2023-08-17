a0f21b94023f43d83001d0bae05099e18d48ebd4
Arina120 <arina.matanskaya@mail.ru>
2023-08-17 11:15:22 +0300

    renamed db and updated queryset

--- advertisement_project/app_advertisement/models.py
+++ advertisement_project/app_advertisement/models.py
@@ -9,3 +9,9 @@ class Advertisement(models.Model):
     connection = models.BooleanField('Связь', help_text='Отметьте, если возможна связь с продавцом', default=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__ (self):
+        return 'Advertisement(id={0}, title={1}, price={2})'.format (self.id, self.title, self.price)
