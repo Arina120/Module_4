ed355174595e57ca06da0c4ee115180c93c8b94c
Arina120 <arina.matanskaya@mail.ru>
2023-08-21 18:46:47 +0300

    added form for creating advertisement

--- advertisement_project/app_advertisement/views.py
+++ advertisement_project/app_advertisement/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -10,3 +13,17 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return HttpResponseRedirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form' : form}
+    return render(request, 'advertisement-post.html', context)
